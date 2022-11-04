62f10a5934b0776e35c6c41ec11035c1626af844
Ade Adeola <adedotster@gmail.com>
2022-11-04 20:47:37 +0000

    add error page

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
-const withPWA = require("next-pwa");
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
 const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
-  dest: "public",
+  trailingSlash: true,
   runtimeCaching,
   disable: process.env.NODE_ENV === "development",
 });
